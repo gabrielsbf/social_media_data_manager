4671f219953d8653dfe500e926d34a7273ea62d5
Zeyou2 <alex_100894@hotmail.com>
2024-03-01 17:24:50 -0300

    implementada uma função que recebe o nome do usuario e indentifica se ele consta no arquivo.

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pandas as pd
 from srcs.classes.social_man import Social_Manager
 from srcs.export_xlsx import *
+from srcs.cfg import *
 
-marica = Social_Manager("section_m")
-face_json = marica.face_post_metrics(file_name='feb_month_face')
-insta_json = marica.insta_metrics(file_name="feb_month_insta")
-marica.writeJsonFile('metrics_face', face_json, JSFILES_PATH)
-marica.writeJsonFile('metrics_insta', insta_json, JSFILES_PATH)
+if __name__ == "__main__":
 
-convertXlsx()
+    def select_user():
+        
+        sectionc = []
+        section = read_sections()
+        while sectionc == []:
+            user_a = input("Digite o Nome do usuario solicitado:")
+            print("input do usuário ", user_a)
+            list_v = list (filter(lambda x: str(x).find("section") >= 0, section))
+            print("lista antes do filtro", list_v)
+            sectionc = list (filter(lambda x: str(x) == user_a, list_v))
+        
+        return (sectionc[0])
+
+    user_selected = select_user()
+    print("User selected is: ", user_selected)
